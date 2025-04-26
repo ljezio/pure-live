bd798b9d0b5b1af4a18f3d00f39df475454941a1
EZIO <1141988108@qq.com>
2025-04-27 01:05:21 +0800

    1.2版本

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pure-douyu
 // @namespace    https://github.com/ljezio
-// @version      1.1
+// @version      1.2
 // @description  斗鱼纯净版
 // @homepage     https://github.com/ljezio/pure-douyu
 // @author       ljezio
@@ -18,27 +18,25 @@
     const root = document.querySelector('#root') ||
         document.querySelector('section[class="layout-Container"]');
     if (!root || !player) return;
-    removeNude(root);
-    // 先把播放器设置为root子节点，再处理播放器
-    root.appendChild(player);
-    clearPayer(player)
-    resize(player);
+    removeNude(root, player);
+    clearPayer(player);
+    dbClick(player);
 
     /**
-     * 移除无用元素
+     * 隐藏无用元素
      */
-    function removeNude(root) {
+    function removeNude(root, player) {
         root.style.padding = '0';
-        const header = document.querySelector('#js-header');
-        if (header) {
-            header.style.display = 'none';
+        root.appendChild(player);
+        for (let node of root.children) {
+            if (!node.isSameNode(player)) {
+                setDisplayNone(node);
+            }
         }
-        root.innerHTML = '';
-        root.appendChild(header);
     }
 
     /**
-     * 清空播放器无用元素
+     * 播放器隐藏无用元素
      */
     function clearPayer() {
         document.querySelector('#js-player-title')?.remove();
@@ -48,24 +46,43 @@
             const element = document.querySelector('div[class^="controlbar-"]');
             if (!element) return;
             element.style.width = '100%';
-            document.querySelector('div[title="主播精彩时刻"]')?.remove();
-            document.querySelector('div[title="问题反馈"]')?.remove();
-            document.querySelector('div[title="显示字幕"]')?.remove();
-            document.querySelector('div[title="多路观看"]')?.remove();
-            document.querySelector('div[title="网页全屏"]')?.remove();
+            setDisplayNone(document.querySelector('div[title="网页全屏"]'));
+            setDisplayNone(document.querySelector('div[title="退出网页全屏"]'));
             clearInterval(interval)
         }, 1000)
+        resizePlayer(player);
     }
 
     /**
      * 播放器自适应宽高
      */
-    function resize(player) {
+    function resizePlayer(player) {
         const windowHeight = innerHeight - 10;
         const height = player.offsetHeight;
         if (height > windowHeight) {
             player.style.width = innerWidth * (windowHeight / height) + 'px'
         }
         player.style.margin = '0 auto';
+    }
+
+    /**
+     * 双击全屏
+     */
+    function dbClick(root) {
+        root.ondblclick = () => {
+            if (!document.fullscreenElement) {
+                document.querySelector('div[title="全屏"]')?.click();
+            } else {
+                document.exitFullscreen().then();
+            }
+        }
+    }
+
+    /**
+     * 隐藏元素
+     */
+    function setDisplayNone(node) {
+        if (!node) return;
+        node.style.display = "none";
     }
 })();
